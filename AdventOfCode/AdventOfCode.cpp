6c560d7e1fa8b07c99f8d307d826e72ecd855108
Jack Robinson <32782230+Niriun@users.noreply.github.com>
2023-01-30 04:21:24 +0000

    import days list in header

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -2,13 +2,13 @@
 //
 
 #include "AdventOfCode.h"
-#include "Days_list.h"
 
 int main()
 {
 	std::vector<Days*>* days = new std::vector<Days*>();
 
 	days->push_back(new Day_1());
+	//days->push_back(new Day_2());
 
 	for (Days* day : *days) {
 		std::cout << day->getDayName() << std::endl << day->getAnswer() << std::endl;
--- AdventOfCode/Days_list.h
+++ AdventOfCode/Days_list.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "Day_1.h"
+#include "Day_2.h"
