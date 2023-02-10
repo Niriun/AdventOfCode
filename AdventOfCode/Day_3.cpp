10f2acf86a09b318f8f22d50032ccb4e5aa6aac0
Jack Robinson <32782230+Niriun@users.noreply.github.com>
2023-02-10 16:19:11 +0000

    it's not quite working for whatever reason

--- AdventOfCode/Day_3.cpp
+++ AdventOfCode/Day_3.cpp
@@ -1,12 +1,18 @@
 #include "Day_3.h"
+#include <iostream>
+#include <map>
 
+const int ASCII_AT_SYMBOL = 64;
+const int SPACE_BETWEEN_ASCII_CASES = 6;
+const int ALPHABET_LENGTH = 26;
 Day_3::Day_3() {
 	std::string input = readInput(_inputFile);
 	parseInput(input);
 }
 
 std::string Day_3::getAnswer() {
-	return "0";
+	calculateRucksackPacking();
+	return std::to_string(part1Answer);
 };
 
 std::string Day_3::getDayName() {
@@ -24,3 +30,53 @@ void Day_3::parseInput(std::string input) {
 	}
 	parsedInput->push_back(input);
 };
+
+void Day_3::calculateRucksackPacking() {
+	int total = 0;
+	int priorityScore = 0;
+	for (std::string line : *parsedInput) {
+		int halfway = line.length() / 2;
+		std::string firstHalf = line.substr(0, halfway);
+		std::string secondHalf = line.substr(halfway, line.length() / 2);
+		std::cout << firstHalf.length() << ", " << secondHalf.length() << std::endl;
+		std::cout << firstHalf << " , " << secondHalf << std::endl;
+		std::set<char>* firstHalfSet = getFirstHalfCharSet(firstHalf);
+		char duplicate = getFirstItemAppearingInBothHalves(firstHalfSet, secondHalf);
+		std::cout << duplicate << std::endl;
+		total += convertCharToScore(duplicate);
+	}
+	std::cout << priorityScore << std::endl;
+	part1Answer = total;
+}
+
+std::set<char>* Day_3::getFirstHalfCharSet(std::string firstHalf) {
+	std::set<char>* set = new std::set<char>();
+	for (size_t i = 0; i < firstHalf.length(); i++) {
+		set->emplace(firstHalf.c_str()[i]);
+	}
+	return set;
+}
+
+char Day_3::getFirstItemAppearingInBothHalves(std::set<char>* firstHalfSet, std::string secondHalf) {
+	for (size_t i = 0; i < secondHalf.length(); i++) {
+		auto ret = firstHalfSet->emplace(secondHalf.c_str()[i]);
+
+		if (!ret.second) {
+			return *ret.first;
+		}
+	}
+	return 0;
+}
+
+int Day_3::convertCharToScore(char character) {
+	character = 'Z';
+	int value = ((int)character) - ASCII_AT_SYMBOL;
+	if (value > ALPHABET_LENGTH) {
+		int retVal = ((value - ALPHABET_LENGTH) - SPACE_BETWEEN_ASCII_CASES);
+		return retVal;
+	}
+	else {
+		int retVal = (value + ALPHABET_LENGTH);
+		return retVal;
+	}
+}
--- AdventOfCode/Day_3.h
+++ AdventOfCode/Day_3.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Days_common.h"
+#include <set>
+
 class Day_3 : public Days
 {
 public:
@@ -11,6 +13,13 @@ protected:
 	void parseInput(std::string input) override;
 
 private:
+
+	void calculateRucksackPacking();
+	std::set<char>* getFirstHalfCharSet(std::string firstHalf);
+	char getFirstItemAppearingInBothHalves(std::set<char>* firstHalfSet, std::string secondHalf);
+	int convertCharToScore(char character);
+
 	const std::string _dayName = "Day 3";
 	const std::string _inputFile = "Day_3.txt";
+	int part1Answer;
 };
